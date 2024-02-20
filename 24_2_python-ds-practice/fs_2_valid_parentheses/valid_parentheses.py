72e3c3d33fd1f51fade1e6a9d0c5c9d95bec9602
yasuhiro.m <yasuhiro@m.com>
2024-02-20 15:48:50 -0800

    adding

---
+++ 24_2_python-ds-practice/fs_2_valid_parentheses/valid_parentheses.py
@@ -0,0 +1,29 @@
+def valid_parentheses(parens):
+    """Are the parentheses validly balanced?
+
+        >>> valid_parentheses("()")
+        True
+
+        >>> valid_parentheses("()()")
+        True
+
+        >>> valid_parentheses("(()())")
+        True
+
+        >>> valid_parentheses(")()")
+        False
+
+        >>> valid_parentheses("())")
+        False
+
+        >>> valid_parentheses("((())")
+        False
+
+        >>> valid_parentheses(")()(")
+        False
+    """
+    if parens[0] == ")" or parens[-1] == "(":
+        return False
+    if not parens.count("(") == parens.count(")"):
+        return False
+    return True
